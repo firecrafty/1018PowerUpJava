1af89f3315364b81ee4bdc3e663d94e97478f221
firecrafty <ryanzblue@gmail.com>
2018-09-02 01:11:06 -0400

    Correct IntakeGripper set value

--- src/main/java/com/pikerobodevils/robot/subsystems/IntakeGripper.java
+++ src/main/java/com/pikerobodevils/robot/subsystems/IntakeGripper.java
@@ -24,7 +24,7 @@ public class IntakeGripper extends Subsystem {
 
     public enum State {
         OPEN(DoubleSolenoid.Value.kReverse),
-        CLOSE(DoubleSolenoid.Value.kReverse);
+        CLOSE(DoubleSolenoid.Value.kForward);
 
         public final DoubleSolenoid.Value value;
 
